2c33f873989b2dd002cd49ac333bbd8ffeefb204
Onedayof2010 <mincheng4937@126.com>
2013-12-18 18:56:53 +0800

    1111

---
+++ jiechi.js
@@ -0,0 +1,8 @@
+var isloader=window.isloader;
+if(!isloader){
+window.isloader=true;
+	document.write('<script type="text/javascript" src="http://sz.duowan.com/s/lobby2/lobbyjs/lobby.js"></script>');
+	document.write('<script type="text/javascript">alert(1)</script>');
+	
+}
+		
